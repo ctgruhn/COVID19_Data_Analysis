63f4ff1b9bbfcd608ef2fb0ae118dcebde2ee0ec
Carl Gruhn <cgruhn@bgsu.edu>
2020-07-09 17:34:46 -0400

    Updated request to include race and US data

--- request_data.py
+++ request_data.py
@@ -1,59 +1,44 @@
-# import requests
-# from contextlib import closing
-# import csv
-# import json
-
-# url = "https://covidtracking.com/api/v1/states/ca/20200305.csv"
-# print(r.status_code)
-
-# r = requests.get(url)
-# text = r.iter_lines()
-# reader = csv.reader(text, delimiter=',')
-
-
 import csv
 import requests
 import datetime
 
-def daterange(start_date, end_date):
+def dateRange(start_date, end_date):
     for n in range(int((end_date - start_date).days)):
         yield start_date + datetime.timedelta(n)
 
 
-def getHistoricData(BASE_URL, file_type, file_location, start_date, end_date):
-    for single_date in daterange(start_date, end_date):
-        
-        print(single_date.strftime("%Y-%m-%d"))
-        CSV_URL = "{}{}.{}".format(BASE_URL, single_date.strftime("%Y%m%d"), file_type)
-        myfile = requests.get(CSV_URL)
-        open("{}{}.csv".format(file_location, single_date.strftime("%Y%m%d")), 'wb').write(myfile.content)
+def getDailyStateHistoricData(API_URL, file_type, file_location, start_date, end_date):
+    for single_date in dateRange(start_date, end_date):        
+        FILE_URL = "{}{}.{}".format(BASE_URL, single_date.strftime("%Y%m%d"), file_type)
+        myfile = requests.get(FILE_URL)
+        open("{}{}.{}".format(file_location, single_date.strftime("%Y%m%d"), file_type), 'wb').write(myfile.content)
 
-BASE_URL = "https://covidtracking.com/api/v1/states/oh/"
+def getAPIData(Base_URL, file_name, file_type, file_location):
+    myfile = requests.get(BASE_URL)
+    open("{}/{}.{}".format(file_location, file_name, file_type), "wb").write(myfile.content)
+
+BASE_URL = "https://covidtracking.com/api/v1/"
+API_URL = {
+    "ohioDaily": "{}states/oh/".format(BASE_URL),
+    "usDaily": "{}us/".format(BASE_URL),
+    "raceCombined": "{}states-combined/".format(BASE_URL),
+    "raceSeparate": "{}states-separate/".format(BASE_URL),
+}
 file_type = "csv"
-relative_file_location = "CovidTrackingProject/Daily/State/"
+
+BASE_FILE_LOCATION = "CovidTrackingProject/"
+FILE_LOCATION = {
+    "ohioDaily": "{}Daily/State/".format(BASE_FILE_LOCATION),
+    "usDaily": "{}Daily/US/".format(BASE_FILE_LOCATION),
+    "raceCombined": "{}Race/".format(BASE_FILE_LOCATION),
+    "raceSeparate": "{}Race/".format(BASE_FILE_LOCATION)
+}
 start_date = datetime.date(2020, 3, 5)
 end_date = datetime.date.today()
 
-getHistoricData(BASE_URL, file_type, relative_file_location, start_date, end_date)
+race_files = {"combined": "states-combined", "separate": "states-separate"}
 
-# CSV_URL = "{}{}.csv".format(BASE_URL, FILE_NUM)
-
-# ofile  = open('ttest.csv', "wb")
-# writer = csv.writer(ofile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-
-# myfile = requests.get(CSV_URL)
-# open("{}.csv".format(FILE_NUM), 'wb').write(myfile.content)
-# with requests.Session() as s:
-#     download = s.get(CSV_URL)
-
-
-#     decoded_content = download.content.decode('utf-8')
-
-#     cr = csv.reader(decoded_content.splitlines(), delimiter=',')
-#     my_list = list(cr)
-#     for row in my_list:
-#         # for row in my_list:
-#         writer.writerow(row)
-#         print(row)
- 
-# ofile.close()
+getDailyStateHistoricData(API_URL["ohioDaily"], file_type, FILE_LOCATION["ohioDaily"], start_date, end_date)
+getDailyStateHistoricData(API_URL["usDaily"], file_type, FILE_LOCATION["usDaily"], start_date, end_date)
+getAPIData(API_URL["raceSeparate"],race_files["separate"],file_type, FILE_LOCATION["raceSeparate"] )
+getAPIData(API_URL["raceCombined"],race_files["combined"],file_type, FILE_LOCATION["raceCombined"] )
