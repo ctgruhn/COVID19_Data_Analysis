a3ea87c15048fc9fdfd656f636520266fa58aac3
Carl Gruhn <cgruhn@bgsu.edu>
2020-07-15 17:02:54 -0400

    Moved constants to config file

--- request_data.py
+++ request_data.py
@@ -1,33 +1,5 @@
 import csv, os.path, requests, datetime
-# import os.path
-# import requests
-# import datetime
-
-# CONSTANTS
-BASE_URL = "https://covidtracking.com/api/v1/"
-API_URL = {
-    "statesDaily": "{}states/".format(BASE_URL),
-    "usDaily": "{}us/".format(BASE_URL),
-    "raceCombined": "{}race/".format(BASE_URL),
-    "raceSeparate": "{}race/".format(BASE_URL),
-}
-STATES = ["ak", "al", "ar", "as", "az", "ca", "co", "ct", "dc", "de", "fl", "ga", "gu", "hi", 
-          "ia", "id", "il", "in", "ks", "ky", "la", "ma", "md", "me", "mi", "mn", "mo", "mp",
-          "ms", "mt", "nc", "nd", "ne", "nh", "nj", "nm", "nv", "ny", "oh", "ok", "or", "pa", 
-          "pr", "ri", "sc", "sd", "tn", "tx", "ut", "va", "vi", "vt", "wa", "wi", "wv", "wy"]
-
-BASE_FILE_LOCATION = "CovidTrackingProject/"
-FILE_LOCATION = {
-    "statesDaily": "{}Daily/State/".format(BASE_FILE_LOCATION),
-    "usDaily": "{}Daily/US/".format(BASE_FILE_LOCATION),
-    "raceCombined": "{}Race/".format(BASE_FILE_LOCATION),
-    "raceSeparate": "{}Race/".format(BASE_FILE_LOCATION)
-}
-RACE_FILES = {"combined": "states-combined", "separate": "states-separate"}
-FILE_TYPE = "csv"
-
-START_DATE = datetime.date(2020, 1, 22)
-END_DATE = datetime.date.today()
+from config import *
 
 def dateRange(start_date, end_date):
     for n in range(int((end_date - start_date).days)):
