9557d47487d9d908685b1b2c89c48c39bc9e15e9
Carl Gruhn <cgruhn@bgsu.edu>
2020-09-11 13:47:56 -0400

    Change default date variable names

--- COVID19_Chart/consolodate.py
+++ COVID19_Chart/consolodate.py
@@ -1,11 +1,11 @@
 # import csv, os.path, datetime
-from COVID19_Chart.config import SUMMARY_FILE_PATH, FILE_TYPE, FILE_LOCATION, STATES, START_DATE, END_DATE
+from COVID19_Chart.config import SUMMARY_FILE_PATH, FILE_TYPE, FILE_LOCATION, STATES, LATEST_UPDATE, TODAY
 
 def dateRange(start_date, end_date):
     for n in range(int((end_date - start_date).days)):
         yield start_date + datetime.timedelta(n)
 
-def setSummary(file_type, file_location, target_file=SUMMARY_FILE_PATH, start_date = START_DATE, end_date = END_DATE):
+def setSummary(file_type, file_location, target_file=SUMMARY_FILE_PATH, start_date = LATEST_UPDATE, end_date = TODAY):
     for single_date in dateRange(start_date, end_date):
         fullFilePath = "{}{}.{}".format(file_location, single_date.strftime("%Y%m%d"), file_type)
         if os.path.isfile(fullFilePath):    # Skips redownloading files
@@ -18,7 +18,7 @@ def setSummary(file_type, file_location, target_file=SUMMARY_FILE_PATH, start_da
             print("{} SUCCESSFULLY ADDED TO SUMMARY".format(fullFilePath))
         else:
             print("{} DOES NOT EXIST".format(fullFilePath))
-def consolodate(start_date=START_DATE, end_date=END_DATE):
+def consolodate(start_date=LATEST_UPDATE, end_date=TODAY):
     for state in STATES:
         file_path = "{}{}/".format(FILE_LOCATION["statesDaily"], state)
         setSummary(FILE_TYPE,file_path)
