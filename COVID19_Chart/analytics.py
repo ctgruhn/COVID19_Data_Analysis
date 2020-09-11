35d73110e24381d0c0dd2f68ecc186d28301cac7
Carl Gruhn <cgruhn@bgsu.edu>
2020-09-11 11:39:20 -0400

    Restructure and remove territories

--- COVID19_Chart/analytics.py
+++ COVID19_Chart/analytics.py
@@ -2,22 +2,32 @@
 # coding: utf-8
 
 # In[1]:
+from COVID19_Chart.config import INITIAL_DATE, LATEST_UPDATE
 import pandas as pd
 import glob
 
 
 # In[2]:
 
-exclude_states = ["PR", "MP", "AU", "GU", "GI"]
+
 
 # In[6]:
+FIELD_NAMES = [
+    "positive", "negative", "pending", "hospitalizedCurrently", "inIcuCurrently", 
+    "onVentilatorCurrently","recovered","hospitalized","totalTestsViral","positiveTestsViral",
+    "negativeTestsViral","positiveCasesViral","deathConfirmed","deathProbable", "death"
+]
+EXCLUDED_TERRITORIES = ["AS", "GU", "MP", "PR", "VI"]
 
 def initData():
     all_data = pd.DataFrame()
-    for f in glob.glob("Data/CovidTrackingProject/Daily/State/*/*.csv"):
+    for f in glob.glob("Data/CovidTrackingProject/Daily/State/*/daily.csv"):
         df = pd.read_csv(f)
         all_data = all_data.append(df,ignore_index=True, sort=False)
     all_data['date'] = pd.to_datetime(all_data['date'], format='%Y%m%d')
+    # Exclude territories. Can separate or make optional in future.
+    all_data = all_data[~all_data['state'].isin(EXCLUDED_TERRITORIES)]
+
     return all_data
 
 # In[7]:
@@ -34,17 +44,16 @@ def filterColumns(data_in_daterange, columns):
 
 # In[40]:
 
-def update_chart(all_data, start_date="03012020", end_date="08121820"):
+def update_chart(all_data, start_date=INITIAL_DATE,  end_date=LATEST_UPDATE):
     s_date = pd.to_datetime(start_date, format='%Y%m%d')
     e_date = pd.to_datetime(end_date, format='%Y%m%d')
     data_in_daterange = filterDate(all_data, s_date, e_date)
-    columns = [
-        "positive", "negative", "pending", "hospitalizedCurrently", "inIcuCurrently", 
-        "onVentilatorCurrently","recovered", "death","hospitalized","totalTestsViral",
-        "positiveTestsViral","negativeTestsViral","positiveCasesViral","deathConfirmed","deathProbable"
-    ]
-    filtered_data = filterColumns(data_in_daterange, columns)
-    filtered_data.to_csv(r'static/data/output.csv')
+
+    filtered_data = filterColumns(data_in_daterange, FIELD_NAMES)
+    filtered_data.to_csv(r'COVID19_Chart/static/data/output.csv')
 
 
 # In[42]:
+all_data = initData()
+update_chart(all_data)
+# %%
